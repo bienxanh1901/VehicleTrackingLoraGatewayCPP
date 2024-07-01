fff687ce23e0b02405ef425067df0c440c62c674
Hai Pham <bienxanhhaiduong@gmail.com>
2024-07-01 07:32:07 +0700

    feat: implement http client

--- cloud_gateway.cpp
+++ cloud_gateway.cpp
@@ -4,15 +4,26 @@
 
 CloudGateway::CloudGateway(std::string url)
 {
+    RestClient::init();
+    
+    m_conn = new RestClient::Connection(url);
+    m_conn->SetTimeout(10);
+    m_conn->SetUserAgent("team/promise");
+
+    m_headers["Content-Type"] = "text/plain";
+    
+    m_conn->SetHeaders(m_headers);
 
 }
 
 CloudGateway::~CloudGateway()
 {
-
+    delete m_conn;
+    RestClient::disable();
 }
 
 bool CloudGateway::send_data(std::string data)
 {
+    m_resp = m_conn->post("/api/v2/device_request", data);
     return true;
 }
